8ca1282d2c1bb71b49ecff7984dace15640788b5
HolySpeeddo <88199882+GianmarcoII@users.noreply.github.com>
2021-09-03 23:36:48 +0200

    Update main.js

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
 $(function(){
+    alert('I do not take credit or rights for the images used on the website. The use of multimedia content is for representative purposes only. All rights go to their respective authors. ');
     const menu = document.querySelector('.menu');
     const mobile = document.querySelector('.mobile');
     powerMenu(menu,mobile);
